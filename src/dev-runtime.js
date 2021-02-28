a604ccb4b226a3af122e224028538fcb71709a6a
Conor Sinclair <conor@sinclair.software>
2021-02-28 23:03:59 +0000

    updated to use full snowsvex plugin

--- src/dev-runtime.js
+++ src/dev-runtime.js
@@ -1,30 +1,51 @@
-let app
-;(async () => {
-	try {
-		const route = pickRoute()
-		const comp = await import(route)
 
-		app = new comp.default({
-			target: document.body,
-			hydrate: true
-		})
-	} catch (err) {
-		console.error(err)
-		const comp = await import('./pages/404.svelte.js')
-		app = new comp.default({
-			target: document.body,
-			hydrate: true
-		})
-	}
-})()
-
-function pickRoute() {
-	const { pathname } = window.location
-	if (pathname === '/') {
-		return './pages/index.svelte.js'
-	}
-	if (pathname.includes('articles')) {
-		return `.${pathname}.svx.js`
-	}
-	return `./pages${pathname}.svelte.js`
-}
+  let app
+  ;(async () => {
+    try {
+      const route = pickRoute()
+      const comp = await import(route)
+  
+      app = new comp.default({
+        target: document.body,
+        hydrate: true
+      })
+    } catch (err) {
+      console.error(err)
+      const comp = await import('./pages/404.svelte.js')
+      app = new comp.default({
+        target: document.body,
+        hydrate: true
+      })
+    }
+  })()
+  
+  function pickRoute() {
+    const { pathname } = window.location
+    if (pathname === '/') {
+      return './pages/index.svelte.js'
+    }
+     
+      if (pathname.includes('pages')) {
+        
+          if (pathname.includes('404')) {
+            return `.${pathname}.svelte.js`
+          }
+        
+          if (pathname.includes('One')) {
+            return `.${pathname}.svelte.js`
+          }
+        
+          if (pathname.includes('Two')) {
+            return `.${pathname}.svelte.js`
+          }
+        
+          if (pathname.includes('index')) {
+            return `.${pathname}.svelte.js`
+          }
+        
+      }
+    
+    return `./pages${pathname}.svelte.js`
+  }
+  
+    
